945323fc2e4dd8c70bc64c6dfd424cb9d5bc2771
Anam Hussain <anamhussain2601@gmail.com>
2021-01-06 23:07:20 +0530

    indexedDB implemented

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,9 @@
-importScripts('/src/js/idb.js')
 
-var CACHE_STATIC_NAME = 'static-v16';
+
+importScripts('/src/js/idb.js');
+importScripts('/src/js/utility.js');
+
+var CACHE_STATIC_NAME = 'static-v17';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 var STATIC_FILES = [
   '/',
@@ -8,7 +11,7 @@ var STATIC_FILES = [
   '/offline.html',
   '/src/js/app.js',
   '/src/js/feed.js',
-  '/src/js/idb .js',
+  '/src/js/idb.js',
   '/src/js/promise.js',
   '/src/js/fetch.js',
   '/src/js/material.min.js',
@@ -20,11 +23,11 @@ var STATIC_FILES = [
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
-var dbPromise = idb.open('feed-store', 1, function (db) {
+var dbPromise = idb.open('posts-store', 1, function (db) {
   if (!db.objectStoreNames.contains('posts')) {
-    db.createObjectStore('post', { keyPath: 'id' })
+    db.createObjectStore('posts', {keyPath: 'id'});
   }
-})
+});
 
 // function trimCache(cacheName, maxItems) {
 //   caches.open(cacheName)
@@ -76,26 +79,29 @@ function isInArray(string, array) {
   }
   return array.indexOf(cachePath) > -1;
 }
+
 self.addEventListener('fetch', function (event) {
 
   var url = 'https://pwagram-be2c7-default-rtdb.firebaseio.com/posts';
   if (event.request.url.indexOf(url) > -1) {
-    event.respondWith( fetch(event.request)
-            .then(function (res) {
-              var clonedRes = res.clone()
-              clonedRes.json().then(function (data){
-                for (var key in data) {
-                  dbPromise.then(function(db) {
-                    var tx = db.transaction( 'posts', 'readwrite')
-                    var store =  tx.objectStore('posts')
-                    store.put(data[key] )
-                    return tx.complete 
-
-                  })
-                }
-              })
-              return res
-            }))
+    event.respondWith(fetch(event.request)
+      .then(function (res) {
+        var clonedRes = res.clone();
+        clonedRes.json()
+          .then(function(data) {
+            for (var key in data) {
+              dbPromise
+                .then(function(db) {
+                  var tx = db.transaction('posts', 'readwrite');
+                  var store = tx.objectStore('posts');
+                  store.put(data[key]);
+                  return tx.complete;
+                });
+            }
+          });
+        return res;
+      })
+    );
   } else if (isInArray(event.request.url, STATIC_FILES)) {
     event.respondWith(
       caches.match(event.request)
