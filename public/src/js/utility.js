b884fe57488361b0e40c35bdb82bc744e76fd5ed
Anam Hussain <anamhussain2601@gmail.com>
2021-01-07 23:02:28 +0530

    indexedDB dlete

--- public/src/js/utility.js
+++ public/src/js/utility.js
@@ -21,3 +21,23 @@ function readAllData(st) {
       return store.getAll( )
   })
 }
+
+function clearAllData(st) {
+  return dbPromise.then(function(db) {
+      var tx = db.transaction(st, 'readwrite')
+      var store = tx.objectStore(st)
+      store.clear()
+      return tx.complete
+  })
+}
+
+function deleteItemFromData(st, id) {
+  return dbPromise.then(function(db) {
+      var tx = db.transaction(st, 'readwrite')
+      var store = tx.objectStore(st)
+      store.delete(id)
+      return tx.complete
+  }).then(()=>{
+    console.log('item deleted ')
+  })
+}
